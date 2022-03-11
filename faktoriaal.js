4fb445ef9725d20c72dfd33f0e91280fcefc2525
Marina Chekmeneva <101263754+Ma1vina@users.noreply.github.com>
2022-03-11 14:08:31 +0300

    Add files via upload

--- faktoriaal.js
+++ faktoriaal.js
@@ -3,6 +3,24 @@ function factori (n) {
     for (x; n > 1; n--) {
 x = x*n;
     }
-console.log(x);
+    console.log (x);
+return x;
 }
-factori(4);
+factori(4); // не совсем правильный вариант, здесь консоль.лог нужно вывести из тела функции
+
+
+
+function factorial(r){
+    let x = 1;
+    if (r == 0 || r == 1){
+      return x;
+    }else{
+      for(let i = r; i >= 1; i--){
+        x = x * i;
+      }
+      return x;
+    }  
+  }
+  let r = 4;
+  x = factorial(r)
+  console.log(x);
