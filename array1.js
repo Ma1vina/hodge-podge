c8034a6cb6ed8de213abd2b1daa7dfad29570c9a
Marina Chekmeneva <101263754+Ma1vina@users.noreply.github.com>
2022-03-15 20:39:24 +0300

    Add files via upload

---
+++ array1.js
@@ -0,0 +1,13 @@
+let arr = [1,5,3,5];
+
+//function
+function y (){
+}
+
+// Method    // (метод - взаимодейстиве с уже существующ.)
+arr.push(3);   //добавить элемент массива в конец
+arr.unshift(10); // добавить вначале
+console.log(arr);
+
+arr.shift();
+console.log(arr);
