894280c02badfaf94582c7a7ae3e00a8779b1c5d
Marina Chekmeneva <101263754+Ma1vina@users.noreply.github.com>
2022-03-23 11:28:23 +0300

    Add files via upload

---
+++ prefix.js
@@ -0,0 +1,9 @@
+// префикс и постфикс
+let num1 = 3; //3
+num1++; //4
+let num2 = num1--;// = 3
+console.log(num1++);//3 постфикс
+console.log(--num2);//3 префикс уменьшился на 1 сразу
+
+console.log(num1);//4 - увеличился на 1
+console.log(num2);//3
