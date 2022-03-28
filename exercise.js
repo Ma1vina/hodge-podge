8866a359e15ad21742d4ab3ffcaf2a21bfda44e9
Marina Chekmeneva <101263754+Ma1vina@users.noreply.github.com>
2022-03-28 17:28:05 +0300

    Add files via upload

---
+++ exercise.js
@@ -0,0 +1,11 @@
+//ПОСЛЕДНИЙ ЭЛЕМЕНТ МАССИВА
+let arr = [1, 2, 3, 4, 8];
+console.log(arr[arr.length-1]);
+
+
+let obj = {a: 1, b: 2, c: 3};
+let key = obj.a;
+console.log(key);
+
+let obj1 = {a: 1, b: 2, c: 3};
+console.log([obj1.length]);
