436c20ed039a2eb5661accce66d5fb3a71d42369
Marina Chekmeneva <101263754+Ma1vina@users.noreply.github.com>
2022-03-29 10:11:26 +0300

    Add files via upload

---
+++ equality.js
@@ -0,0 +1,26 @@
+//РАВЕНСТВО
+//Если переменная test равна 10, то выведите на экран слово 'верно', иначе выведите слово 'неверно'.
+let test = 6;
+if(test == 10){
+    console.log("verno")
+}else{
+    console.log("ne verno")
+}
+
+//НЕРАВЕНСТВО
+//1)Если переменная test не равна 10, то выведите на экран слово 'верно', иначе выведите слово 'неверно'.
+let test1 = 8;
+if(test != 10){
+    console.log("verno")
+}else{
+    console.log("ne verno")
+}
+
+//2)
+let test4 = 3;
+let test2 = 2;	
+if (test4 !== test2) {
+	console.log('верно');// выведется
+} else {
+	console.log('неверно');
+}
