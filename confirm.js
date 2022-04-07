19173efab1af236c36da6e814dc19a8645ea1843
Marina Chekmeneva <101263754+Ma1vina@users.noreply.github.com>
2022-04-07 17:42:33 +0300

    Add files via upload

---
+++ confirm.js
@@ -0,0 +1,66 @@
+// let age = prompt("Сколько вам лет?");
+
+// if (age>=18) {
+//   alert( "Доступ получен" );
+// } else {
+//   alert( 'Доступ закрыт' );
+// }
+// Проверка ошибок. Задачка 1
+//проверить сумму чисел:
+// let num1 = 1;
+// let num2 = 2;
+// if (num1 + num2 == 3) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+//сумма чисел
+// let num1 = '1';
+// let num2 = '2';
+
+// if ((Number(num1) + Number(num2)) == 3) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+//или
+// let num1 = '1';
+// let num2 = '2';
+
+// if (+num1 + +num2 == 3) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+// let num = 123;
+// num = String(num);
+// if (num[0] == 1) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+// let num = 123;
+// if (num = String(num),num[0] == 1) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+//  что в числе 2 цифры
+// let num = 12;
+// num = String(num)
+// if (num.length == 2) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+//что два элемента
+let num = 12;
+if (num =String(num),num.length == String(2)) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
