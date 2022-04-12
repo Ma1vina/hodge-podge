053196a7d7888d24e2d3d7770912eca32642d3f5
Marina Chekmeneva <101263754+Ma1vina@users.noreply.github.com>
2022-04-12 15:07:13 +0300

    Add files via upload

--- push.js
+++ push.js
@@ -1,5 +1,23 @@
+// let arr = [];
+// for (let i = 1; i <= 5; i++) {
+// 	arr.push(i);
+// }
+// console.log(arr); // выведет [1, 2, 3, 4, 5]
+
+//Используя цикл и метод push заполните массив 10-ю буквами 'x'.
 let arr = [];
-for (let i = 1; i <= 5; i++) {
+for (let i = 0; i <=9; i++) {
+	arr[i] = "х";
 	arr.push(i);
 }
-console.log(arr); // выведет [1, 2, 3, 4, 5]
+console.log(arr); 
+
+//запишем в новый массив только элементы с четными числами:
+let arr = [1, 2, 3, 4, 5];
+let result = [];
+for (let elem of arr) {
+	if (elem % 2 == 0) {
+		result.push(elem);
+	}
+}
+console.log(result); // выведет [2, 4]
