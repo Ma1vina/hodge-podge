60653d5c2529af93234a50bc27ea1c9d388961e2
Marina Chekmeneva <101263754+Ma1vina@users.noreply.github.com>
2022-04-18 17:52:10 +0300

    Add files via upload

--- objMulti.js
+++ objMulti.js
@@ -20,3 +20,33 @@
 // }
 // let summ = obj["key1"]["a"] + obj["key1"]["b"] + obj["key1"]["c"]["d"] +obj["key1"]["c"]["e"] + obj["key1"]["f"]+ obj["key2"]["g"]+obj["key2"]["h"];
 // console.log(summ);
+
+
+//ПЕРЕБОР ЭЛЕМЕНТОВ
+// let obj = {
+// 	a: {1: 'a1',2: 'a2',3: 'a3',},
+//     b: {1: 'b1',2: 'b2',3: 'b3',},
+// 	c: {1: 'c1',2: 'c2',3: 'c3',},
+// };
+// for (let key in obj) { // ключи в объекте
+// 	let subObj = obj[key]; // мини-объекты (=) в элементах ключей
+// 	for (let subKey in subObj) { // и ключики из мини-объектов
+// 		console.log(subObj[subKey]); //мини-объекты( и их мини-ключики)
+// 	}
+// }
+
+
+//Задачка1 . Используя циклы, найдите сумму элементов этого объекта.
+let obj = {1: {1: 11, 2: 12, 3: 13,},
+	       2: {1: 21, 2: 22, 3: 23,},
+	       3: {1: 24, 2: 25, 3: 26,},
+          };
+  let summ = 0;
+  for(let key in obj){
+      let subObj = obj[key];
+      for(let subKey in subObj){
+          console.log(subObj[subKey])
+          summ = summ + subObj[subKey]
+      }
+  }        
+console.log(summ);
