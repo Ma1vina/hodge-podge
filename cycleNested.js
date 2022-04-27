0b68154f97a54b483081692a07834132ea3a9efd
Marina Chekmeneva <101263754+Ma1vina@users.noreply.github.com>
2022-04-27 11:39:40 +0300

    Add files via upload

---
+++ cycleNested.js
@@ -0,0 +1,10 @@
+
+	    let arr = [];
+	    for (let i = 0; i < 5; i++) {
+            let vewarr = [];
+   for (let j = i+1  ; j < 5; j++) {// i + 1 после первого индекса(вторым числом)
+	   
+       vewarr.push([i,j])
+      
+    }
+	    }
