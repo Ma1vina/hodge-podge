8d8a63e3aed6d8ce75f61f6b4da7524a91e3ec0c
Marina Chekmeneva <101263754+Ma1vina@users.noreply.github.com>
2022-05-11 17:40:58 +0300

    Add files via upload

---
+++ func-recursion.js
@@ -0,0 +1,48 @@
+//С помощью рекурсии выведите элементы этого массива на экран.
+// let arr = [1, 3, 3, 4, 5];
+// let i = 0;
+
+// function getElem (arr){
+//     console.log(arr[i]);
+//     i++;
+//     if (i < arr.length){
+//        if(i !== undefined){
+//   getElem(arr);
+//  }
+// }
+// };
+// console.log(getElem(arr));
+
+//сумма элементов
+// function getSum(arr) {
+// 	let sum = arr.shift();
+	
+// 	if (arr.length !== 0) {
+// 		sum += getSum(arr);
+// 	}
+	
+// 	return sum;
+// }
+
+// console.log(getSum([1, 2, 3]));
+
+
+//С помощью рекурсии найдите сумму квадратов элементов этого массива.
+ let arr = [2, 2, 3, 4, 5];
+
+function getSum(arr) {
+	let sum = getSquare(arr.shift());
+	if (arr.length !== 0) {
+        sum += getSum(arr);
+	}
+	
+	return sum;
+}
+    function getSquare (arr){
+        let sum = 0;
+        for(let elem of arr){
+        sum = elem*elem
+        }
+        return sum;
+     }
+ console.log(getSum([1,3]));
