ba4f68f2e9ae8c9c826f49c999a1d19c401bb7fa
Marina Chekmeneva <101263754+Ma1vina@users.noreply.github.com>
2022-05-12 13:07:07 +0300

    Add files via upload

--- func-Arrow.js
+++ func-Arrow.js
@@ -1,10 +1,34 @@
+"use strict"
 //Задача 1/ Дана следующая функция с коллбэком: Упростите коллбэк через стрелочную функцию.
 // let result = every([1, 2, 3, 4, 5], function(elem) { if (elem > 0) { return true; } else { return false; } });
 
-// let result = every([1, 2, 3, 4, 5], elem => elem > 0);
+
+// let arr = [1, 2, 3, -4, -5];
+
+//  let callback = elem => {
+//      return elem>0;
+//  }
+
+//  let getDigit = (array, arriveCallback) => {
+//      for(let i=0; i<array.length; i++){
+//          if(arriveCallback(array[i])){
+//              console.log(array[i])
+//          }
+//      }
+//  }
+//  getDigit(arr,callback);
 
 
-//Задача 2.
+///////////////////////////////////////////////
+// let func2 = (num1, num2) => { 
+//  return num1 * num2; 
+// } 
+// console.log(func2(2,2)); 
+//////////////////////////////////////////
+
+
+//Задача 2.Упростите коллбэк через стрелочную функцию.
+
 // let result = every([1, 2, 3, 4, 5], function(elem, index) {
 // 	if (elem * index > 10) {
 // 		return true;
@@ -14,4 +38,16 @@
 // });
 
 
-//let result = every([1,2,3,4,5], (elem,index) => elem * index > 10);
+//  let callback = (elem,index) => elem*index>10;
+ 
+//  let getDigit = (array, funcCallbacks) => {
+//      for(let i =0; i<array.length;i++){
+//          if(funcCallbacks(array[i],i)){
+//              console.log(array[i])
+//          }
+//      }
+//  }
+
+//  let arr = [1, 2, 3, 4, 50];
+ 
+// getDigit(arr,callback);
