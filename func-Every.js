f5b5f8922e76ce271a116a09492473f157bed51e
Marina Chekmeneva <101263754+Ma1vina@users.noreply.github.com>
2022-05-17 16:09:25 +0300

    Add files via upload

---
+++ func-Every.js
@@ -0,0 +1,11 @@
+//Задача 1.Дан массив с числами. Проверьте то, что все элементы в массиве больше нуля.
+let arrm = [1,35,-4,5]
+// let result = arrm.every(elem => elem>0);
+// console.log(result);
+
+//Задача 2. Дан массив с числами. Проверьте то, что для всех элементов произведение их значений на их порядковый номер меньше 30.
+// let result = arrm.every(function(elem,index) {
+//     let sum = elem*index;
+// return sum<30   
+// });
+// console.log(result);
