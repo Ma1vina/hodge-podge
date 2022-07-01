d3362123ba11f8ca28f129d1b2ec1b014e91fa56
Marina Chekmeneva <101263754+Ma1vina@users.noreply.github.com>
2022-07-01 17:24:21 +0300

    Add files via upload

--- destructuring.js
+++ destructuring.js
@@ -32,11 +32,68 @@
 // console.log(time);  
 
 //
-function func() {
-	return (new Date).getDate();
-}
-let arr = [2022, 6];
-let [year, month, day = func()] = arr; //значение по умолчанию - текущ день неделти
+// function func() {
+// 	return (new Date).getDate();
+// }
+// let arr = [2022, 6];
+// let [year, month, day = func()] = arr; //значение по умолчанию - текущ день неделти
 
-console.log(day);
+// console.log(day);
+
+
+//
+// function func() {
+// 	return (new Date).getFullYear();
+// }
+
+// let obj = {
+// 	month: 12,
+// 	day:   31,
+// };
+
+// let {year = func(), month, day} = obj;
+
+// console.log(year);
+
+//
+// function func() {
+// 	return obj = {
+// 		year:  2025,
+// 		month: 12,
+// 		day:   31,
+// 	};
+// }
+
+// let {year, month, day} = func();
+
+
+//
+// func([2025, 12, 31]);
+
+// function func([year, month, day]) {
+// 	console.log(year);  // выведет 2025
+// 	console.log(month); // выведет 12
+// 	console.log(day);   // выведет 31
+// }
+
+//Переделайте следующий код через деструктуризацию согласно изученной теории:
+// function func([name,surname,info]) {
+// 	console.log(name);
+// 	console.log(surname);
+// 	console.log(info);
+// }
+
+// func( ['Иван', 'Иванов', 'отдел разработки', 'программист', 2000] );
+
+//переделайте следующий код через деструктуризацию согласно изученной теории:
+
+function func([name,surname,info=1]) {
+		console.log(name);
+	console.log(surname);
+	console.log(info);
+
+}
+
+let arr =['Иван', 'Иванов']
+func(arr);
 
