9cf1f6ecf06dd060ef5323bb2078dc09d4ab5ae5
Marina Chekmeneva <101263754+Ma1vina@users.noreply.github.com>
2022-08-09 17:00:49 +0300

    Add files via upload

---
+++ reduce.js
@@ -0,0 +1,19 @@
+// let arr = [1, 2, 3, 4, 5, 6];
+
+// let result = arr.reduce(function(sum, elem) {
+// 	return sum + elem;
+// }, 0);
+
+// console.log(result);
+
+// let arr = [1, -2, -3, 4, 5, -6];
+
+// let result = arr.reduce(function(sum, elem) {
+// 	if (elem >= 0) {
+// 		return sum + elem;
+// 	} else {
+// 		return sum;
+// 	}
+// });
+
+// console.log(result);
