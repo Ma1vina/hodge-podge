c58419ad866b11e2036661900b8075a7ff59b068
Marina Chekmeneva <101263754+Ma1vina@users.noreply.github.com>
2022-08-09 14:18:54 +0300

    Add files via upload

---
+++ json/json1.js
@@ -0,0 +1,40 @@
+"use strict"
+// 1
+// let json = '[1, 2, 3, 4, 5]'; 
+// let arr = JSON.parse(json); 
+// console.log(arr);
+
+//Преобразуйте эту строку в настоящий массив JavaScript и найдите сумму чисел этого массива.
+// let summ = 0;
+// for(let i=0; i<arr.length;i++){
+//     summ = summ +arr[i]
+// }
+// console.log(summ);
+
+
+// 2
+// Дана строка в формате JSON, содержащая объект с данными:
+// let json = `{
+// 	"data1": [1,2,3],
+// 	"data2": [4,5,6],
+// 	"data3": [7,8,9]
+// }`;
+// // Найдите сумму чисел из представленных данных
+
+// let obj = JSON.parse(json)
+// console.log(obj);
+// // { data1: [ 1, 2, 3 ], data2: [ 4, 5, 6 ], data3: [ 7, 8, 9 ] }
+// let summ = 0;
+// for(let key in obj){
+//    let arr = obj[key]
+//  for(let i = 0;i<arr.length;i++){
+//        summ = summ + arr[i];
+//         }
+//     }
+// console.log(summ);
+
+// 3
+// Дана строка в формате JSON, содержащая имена юзеров:
+
+ let json = '["user1","user2","user3","user4","user5"]';
+// Выведите эти имена в виде списка ul.
