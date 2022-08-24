6aaccddd6ba30e498641b07daf5b9b1695984c2b
Marina Chekmeneva <101263754+Ma1vina@users.noreply.github.com>
2022-08-24 10:57:18 +0300

    Add files via upload

--- practiceDOM/table.js
+++ practiceDOM/table.js
@@ -87,27 +87,65 @@
 // }
 
 // 6
-let employees = [
-	{name: 'employee1', age: 30, salary: 400},
-	{name: 'employee2', age: 31, salary: 500},
-	{name: 'employee3', age: 32, salary: 600},
-];
-// Выведите элементы этого массива в виде HTML таблицы.
+// let employees = [
+// 	{name: 'employee1', age: 30, salary: 400},
+// 	{name: 'employee2', age: 31, salary: 500},
+// 	{name: 'employee3', age: 32, salary: 600},
+// ];
+// // Выведите элементы этого массива в виде HTML таблицы.
 
-let table = document.querySelector("#table");
-for(let user of employees){
-    let tr = document.createElement("tr");
-    let td1= document.createElement("td");
-    td1.textContent = user.name;
-    tr.appendChild(td1);
+// let table = document.querySelector("#table");
+// for(let user of employees){
+//     let tr = document.createElement("tr");
+//     let td1= document.createElement("td");
+//     td1.textContent = user.name;
+//     tr.appendChild(td1);
 
-    let td2 = document.createElement("td");
-    td2.textContent = user.age;
-    tr.appendChild(td2)
+//     let td2 = document.createElement("td");
+//     td2.textContent = user.age;
+//     tr.appendChild(td2)
 
-    let td3 = document.createElement("td");
-    td3.textContent = user.salary;
-    tr.appendChild(td3);
+//     let td3 = document.createElement("td");
+//     td3.textContent = user.salary;
+//     tr.appendChild(td3);
 
-    table.appendChild(tr);
-}
+//     table.appendChild(tr);
+// }
+
+// 7
+// Сделайте кнопку, по нажатию на которую в таблицу будет добавляться новый ряд.
+
+// let table = document.querySelector("#table");
+// let btn = document.querySelector("button");
+
+// btn.addEventListener("click", function(){
+//     let tr = document.createElement("tr");
+//     for (let i = 1; i <= 1; i++) {
+//         let td = document.createElement('td');
+//         tr.appendChild(td);
+//     }
+    
+//     table.appendChild(tr);
+// })
+
+// 8
+//Дана также кнопка. Сделайте так, чтобы по нажатию на кнопку таблица увеличивалась на один ряд и на одну колонку.
+// let trS = document.querySelectorAll("#table tr");
+// let btn= document.querySelector("button");
+
+// btn.addEventListener("click",function(){
+// for(let tr of trS){
+//     let td = document.createElement("td");
+//     tr.appendChild(td);
+// }
+// })
+
+
+// 8.1
+// Пусть дана некоторая HTML таблица #table. Пусть перед нами стоит задача что-то сделать с каждой ячейкой таблицы,
+//  например, записать в каждую из них текст '!'.
+// let tds = document.querySelectorAll('#table td');
+
+// for (let td of tds) {
+// 	td.textContent = '!';
+// }
