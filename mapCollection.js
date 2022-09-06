873abb05971ef2fd9799b397ec0143c4d6619caf
Marina Chekmeneva <101263754+Ma1vina@users.noreply.github.com>
2022-09-06 15:51:54 +0300

    Add files via upload

---
+++ mapCollection.js
@@ -0,0 +1,51 @@
+//1
+// Пусть даны 3 массива. Создайте коллекцию Map, сделайте ключами коллекции эти массивы, 
+// а значениями - какие-нибудь строки.
+// let arr1 = [1,2,4];
+// let arr2 = [2,4,5];
+// let arr3 = [3,4,5];
+
+// let map = new Map;
+// map.set(arr1, "data1");
+// map.set(arr2, "data2");
+// map.set(arr3, "data3");
+
+// console.log(map.get(arr1));
+// console.log(map.get(arr2));
+// console.log(map.get(arr3));
+
+// 2
+// Пусть даны 1 объекта и 1 массива. Создайте коллекцию Map, сделайте ключами коллекции объекты, 
+// а значениями - соответствующие массивы.
+// let obj1 = {1: "sss", 2: "ddd", 3: "rrr"};
+// let arr1 = [1,1,1];
+
+// let map = new Map;
+// map.set(obj1, arr1);
+
+// console.log(map.get(obj1));
+
+// 3
+// Пусть дана коллекция Map. Получите массив ее ключей и переберите их циклом for-of.
+let map = new Map;
+
+let obj =[
+    ["огурец", 500],
+    ["помидор", 350],
+    ["лук",    50]
+  ];
+  
+  // перебор по ключам;
+  for (let vegetable of obj.keys()) {
+    console.log(vegetable); // огурец, помидор, лук
+  }
+  
+  // перебор по значениям (числа)
+  for (let amount of obj.values()) {
+    console.log(amount); // 500, 350, 50
+  }
+  
+  // перебор по элементам в формате [ключ, значение]
+  for (let entry of obj) { 
+    console.log(entry); // огурец,500 (и так далее)
+  }
