07aa96e668b15219b55061483aea70d91ef41b93
Marina Chekmeneva <101263754+Ma1vina@users.noreply.github.com>
2022-09-14 01:34:05 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-"use strict";
-  let y = 0; 
-  let arr = [-5, -3, 5, 10]; 
-  let summ = 0; 
-  for (let i = 0; i < arr.length; i++) { 
-   if (y > arr[i]) { 
-   } else { 
-   summ = summ + arr[i]; 
-   } 
-  } 
-  console.log(summ);
+import moment from 'moment';
+console.log("Hello from JavaScript!");
+console.log(moment().startOf('day').fromNow());
+var name = "pretty1", time = "today";
+console.log(`Hello ${name}, how are you ${time}?`);
