c155cab2dbd0c9a1a89b5fd6febf735282e98ba8
Marina Chekmeneva <101263754+Ma1vina@users.noreply.github.com>
2022-09-14 01:40:40 +0300

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-import moment from 'moment';
-console.log("Hello from JavaScript!");
-console.log(moment().startOf('day').fromNow());
-var name = "pretty1", time = "today";
-console.log(`Hello ${name}, how are you ${time}?`);
